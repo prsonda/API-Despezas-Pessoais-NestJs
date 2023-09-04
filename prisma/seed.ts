5df562e707c1344aaa2e405ac968165f43501cb3
prsonda <prsonda@gmail.com>
2023-09-04 16:34:07 -0300

    Seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,35 +3,90 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  const categories = [
-    'Alimentação',
-    'Assinaturas e Serviços',
-    'Aluguel',
-    'Mercado',
-    'Cuidados Pessoais',
-    'Educação',
-    'Família',
-    'Lazer',
-    'Pets',
-    'Presentes',
-    'Roupas',
-    'Saúde',
-    'Transporte',
-    'Salário',
-    'Vendas',
-    'Outras receitas',
-    'Outras despesas',
+  const categoryData = [
+    {
+      name: 'Alimentação',
+      description: 'Alimentos e bebidas',
+    },
+    {
+      name: 'Assinaturas e Serviços',
+      description: 'TV, internet, celular, etc',
+    },
+    {
+      name: 'Aluguel',
+      description: 'Aluguel, condomínio, etc',
+    },
+    {
+      name: 'Mercado',
+      description: 'Supermercado, padaria, etc',
+    },
+    {
+      name: 'Cuidados Pessoais',
+      description: 'Academia, cabeleireiro, etc',
+    },
+    {
+      name: 'Educação',
+      description: 'Cursos, livros, etc',
+    },
+    {
+      name: 'Família',
+      description: 'Despesas com filhos, etc',
+    },
+    {
+      name: 'Lazer',
+      description: 'Viagens, bares, restaurantes, etc',
+    },
+    {
+      name: 'Pets',
+      description: 'Ração, veterinário, etc',
+    },
+    {
+      name: 'Presentes',
+      description: 'Presentes para amigos e familiares',
+    },
+    {
+      name: 'Roupas',
+      description: 'Roupas, calçados, etc',
+    },
+    {
+      name: 'Saúde',
+      description: 'Plano de saúde, remédios, etc',
+    },
+    {
+      name: 'Transporte',
+      description: 'Ônibus, metrô, uber, etc',
+    },
+    {
+      name: 'Salário',
+      description: 'Salário recebido',
+    },
+    {
+      name: 'Vendas',
+      description: 'Vendas de mercadorias e serviços',
+    },
+    {
+      name: 'Outras receitas',
+      description: 'Outras receitas',
+    },
+    {
+      name: 'Outras despesas',
+      description: 'Outras despesas',
+    },
   ];
 
-  for (let category in categories) {
-    await prisma.categoria.create({
-      data: {
-        descricao: category,
-      },
+  for (const data of categoryData) {
+    await prisma.categories.create({
+      data,
     });
   }
+
+  console.log('Seed da tabela "categories" criada com sucesso.');
 }
 
-main();
-
-prisma.$disconnect();
+main()
+  .catch((error) => {
+    console.error('Erro ao criar a seed:', error);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
