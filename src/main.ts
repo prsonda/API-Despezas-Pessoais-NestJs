eda4728aa79aca7799c21cb5bc8804f24146b222
prsonda <prsonda@gmail.com>
2023-09-04 20:37:15 -0300

    Implantação da documentação com swagger

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { ExpressAdapter } from '@nestjs/platform-express';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'; // Importe o SwaggerModule e o DocumentBuilder
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 
@@ -11,6 +12,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, new ExpressAdapter(), {
     cors: true,
   });
+
+  // Configuração do Swagger
+  const options = new DocumentBuilder()
+    .setTitle('API Despesas Pessoais')
+    .setDescription('API para gerenciamento de despesas pessoais')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('doc', app, document);
 
   await app.listen(port, () =>
     console.log(`Server is running on port ${port}`),
